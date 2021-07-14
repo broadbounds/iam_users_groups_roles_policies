326be017bfe7824883e27a70bf69371e1a6bd376
broadbounds <84296908+broadbounds@users.noreply.github.com>
2021-07-14 00:24:30 -0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,14 @@
 output "user_arn" {
+ description = "The ARN assigned by AWS for this user"
  value = "${aws_iam_user.employee.*.arn}"
+}
+
+output "user_access_key_id" {
+  description = "The access key ID"
+  value = aws_iam_access_key.this.*.id
+}
+
+output "user_access_key_secret" {
+  description = "The access key secret"
+  value       = aws_iam_access_key.this_no_pgp.*.secret
 }
