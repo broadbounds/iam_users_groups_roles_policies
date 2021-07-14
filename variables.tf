8c55b4c21665e6a1a17625bcdd256cc8146b49c2
broadbounds <84296908+broadbounds@users.noreply.github.com>
2021-07-13 23:45:25 -0700

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+  type = string
+  default = "us-east-2"
+}
+
+variable "access_key" {
+  type        = string
+  default     = ""
+}
+
+variable "secret_key" {
+  type        = string
+  default     = ""
+}
